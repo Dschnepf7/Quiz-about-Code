33ea23a456153e2439a2d2dcfaaf9f25516782dd
Dschnepf7 <domschnepf1@gmail.com>
2023-01-31 16:08:39 -0600

    added startQuiz and startTimer functions to js

---
+++ Assets/JS/script.js
@@ -0,0 +1,71 @@
+//click button to start
+    //event listener
+    //a timer will start
+//user is presented with a question
+//user selects one multiple choice question
+    //IF answer is wrong, decrement the timer
+        //Display that the user choice was incorrect, then move to next question
+    //IF answer is correct
+        //Display that the user choice was correct(textContent)
+        //Increment the score++
+    //FOR loop to loop through the questions array
+//When timer hits 0 OR no more questions in array
+    //THEN clear interval
+    //THEN allow user to input initials
+    //THEN link to highscores page(two separate HTML pages, index.html and hiscores.html)
+    //Save data to local storage(setItem, getItem)
+
+//startQuiz() function changes content
+    //hide the start screen
+    //unhide the questions section
+    //start the timer
+    //show the starting time
+    //call getQuestion()
+//getQuestion() function get current question object from questions array
+    //change content of the page
+    //getElementbyId, change textContent
+    //loop over choices create a button for each choice
+    //after creating all the elements we appendChild
+//questionClick() function that checks choice user clicked
+    //first we check that the event.target matches an answer choice
+    //check event.target.value matches the questions[currentIndex].answer
+    var startButton = document.querySelector(".startButton");
+    var timerElement = document.querySelector(".timer-count");
+    var questionSpace = document.querySelector(".questionSpace");
+    var timer;
+    var timerCount;
+
+    var questions = [
+        {
+            question: "What does HTML stand for?",
+            choices: ["Hyper text Martian language", "Hyper text markup language"],
+            answer: "Hyper text markup language",
+        },
+        {
+            question: "What does CSS stand for?",
+            choices: ["Cats stay sleeping", "Cascading style sheets"],
+            answer: "Cascading style sheets",
+        }
+    ]
+
+    function startQuiz(){
+        timerCount = 60;
+        startTimer();
+
+    };
+
+    function startTimer(){
+        timer = setInterval(function(){
+            timerCount--;
+            timerElement.textContent = timerCount;
+            if(timerCount >=0){
+                clearInterval(timer);
+            }
+        }, 1000);
+    };
+       
+        
+    
+        
+
+    startButton.addEventListener("click", startQuiz);
